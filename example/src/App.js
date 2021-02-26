39bf99389b50e17c253f8711451edf62ef8ef27e
hoangghaii <46194425+hoangghaii@users.noreply.github.com>
2021-02-26 12:04:30 +0700

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-popup-notify'
+import 'react-popup-notify/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
